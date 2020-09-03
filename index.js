6a6aee6b4b0e865b84d4f3867cec1565e3df65ca
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-03 13:02:54 -0400

    mp

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+// heroku will add a port variable ot the environment
+// we can read it from there using the process.env object
+
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
